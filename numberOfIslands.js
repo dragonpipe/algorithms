a3aeae263451380f2440bfae0f7a552ba908663c
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-11-25 21:10:53 -0400

    Developed islands work

--- numberOfIslands.js
+++ numberOfIslands.js
@@ -5,8 +5,6 @@ function numberOfIslands(grid) {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       if (grid[i][j] == 1 && !booleanGrid[i][j]) {
-        booleanGrid[i][j] = true;
-
         booleanGrid = countIsland(i, j, grid, booleanGrid);
         resul++;
       }
@@ -25,28 +23,24 @@ let grid = [
 
 function countIsland(fil, col, grid, booleanGrid) {
   //countIsland returns the booleanGrid modified
-
+    booleanGrid[fil][col] == true
   if (col != grid.length - 1) {
     if (grid[fil][col + 1] == 1 && !booleanGrid[fil][col + 1]) {
-      booleanGrid[fil][col + 1] = true;
       booleanGrid = countIsland(fil, col + 1, grid, booleanGrid);
     }
   }
   if (fil != grid.length - 1) {
     if (grid[fil + 1][col] == 1 && !booleanGrid[fil + 1][col]) {
-      booleanGrid[fil + 1][col] = true;
       booleanGrid = countIsland(fil + 1, col, grid, booleanGrid);
     }
   }
   if (col != 0) {
     if (grid[fil][col - 1] == 1 && !booleanGrid[fil][col - 1]) {
-      booleanGrid[fil][col - 1] = true;
       booleanGrid = countIsland(fil, col - 1, grid, booleanGrid);
     }
   }
   if (fil != 0) {
     if (grid[fil - 1][col] == 1 && !booleanGrid[fil - 1][col]) {
-      booleanGrid[fil - 1][col] = true;
       booleanGrid = countIsland(fil - 1, col, grid, booleanGrid);
     }
   }
